815d7bc5f0eb0dd9236e801a81688c6b818e1a5d
dewmithmihisara <mihisaralokuhewage@>
2023-03-28 03:06:01 +0530

    sign in 1.0.1

--- project_dkf/src/main/java/lk/ijse/project_dkf/controller/LogIn.java
+++ project_dkf/src/main/java/lk/ijse/project_dkf/controller/LogIn.java
@@ -1,4 +1,22 @@
 package lk.ijse.project_dkf.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class LogIn {
+    @FXML
+    private Button frgtPwBtn;
+
+    @FXML
+    private TextField pwTxt;
+
+    @FXML
+    private Button sgnBtn;
+
+    @FXML
+    private Button sgnUpBtn;
+
+    @FXML
+    private TextField usrTxt;
 }
