42029c4b7c12a7a4172a449563e8cf2983f13075
dewmithmihisara <mihisaralokuhewage@>
2023-03-28 22:18:10 +0530

    sign in 2.0.0

--- project_dkf/src/main/java/lk/ijse/project_dkf/controller/LogInFormController.java
+++ project_dkf/src/main/java/lk/ijse/project_dkf/controller/LogInFormController.java
@@ -1,42 +1,4 @@
 package lk.ijse.project_dkf.controller;
-
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
 public class LogInFormController {
-    @FXML
-    private Button frgtPwBtn;
 
-    @FXML
-    private TextField pwTxt;
-
-    @FXML
-    private AnchorPane root;
-
-    @FXML
-    private Button sgnBtn;
-
-    @FXML
-    private Button sgnUpBtn;
-
-    @FXML
-    private TextField usrTxt;
-
-    @FXML
-    void signUpOnActon(ActionEvent event) throws IOException {
-        Parent parent = FXMLLoader.load(getClass().getResource("/view/newAcForm.fxml"));
-        Stage stage = (Stage) root.getScene().getWindow();
-        stage.setTitle("Create new Account");
-        stage.setScene(new Scene(parent));
-        stage.centerOnScreen();
-    }
 }
