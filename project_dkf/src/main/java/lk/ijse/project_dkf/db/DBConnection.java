b8083372f398659cc2643584555e80ac80473d55
dewmithmihisara <mihisaralokuhewage@>
2023-04-30 11:29:22 +0530

    DKF 1.0.8

--- project_dkf/src/main/java/lk/ijse/project_dkf/db/DBConnection.java
+++ project_dkf/src/main/java/lk/ijse/project_dkf/db/DBConnection.java
@@ -3,16 +3,20 @@ package lk.ijse.project_dkf.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConnection {
     private static DBConnection dbConnection;
+    private static Properties props=new Properties();
+    private static final String URL="jdbc:mysql://localhost:3306/dkf";
+
+    static {
+        props.setProperty("user","root");
+        props.setProperty("password", "Dew@2005");
+    }
     private Connection con;
     private DBConnection() throws SQLException {
-        con= DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/dkf",
-                "root",
-                "Dew@2005"
-        );
+        con=DriverManager.getConnection(URL,props);
     }
     public static DBConnection getInstance() throws SQLException {
         return (null == dbConnection)? dbConnection=new DBConnection(): dbConnection;
